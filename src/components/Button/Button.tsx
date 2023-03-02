f3582568feed4cf530cadc81b3e19f7c25d6f644
Victor Granda <victorgranda1992@gmail.com>
2023-03-02 13:06:18 +0100

    Update Button Component to accept type and disabled by props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,17 @@ import { ButtonStyled } from "./ButtonStyled";
 
 export interface ButtonProps {
   text: string;
+  type: string;
+  disabled: boolean;
 }
 
-const Button = ({ text }: ButtonProps): JSX.Element => {
-  return <ButtonStyled>{text}</ButtonStyled>;
+const Button = ({ text, type, disabled }: ButtonProps): JSX.Element => {
+  return (
+    <div className="button__container">
+      <ButtonStyled>{text}</ButtonStyled>
+      <span className="required">Sorry, something went wrong</span>
+    </div>
+  );
 };
 
 export default Button;
