a498f5f8fd00d8abc7f6673c1d87db864c02d5d9
Victor Granda <victorgranda1992@gmail.com>
2023-03-02 13:20:07 +0100

    Add test for EventRegister

--- src/components/Form/EventRegister.test.tsx
+++ src/components/Form/EventRegister.test.tsx
@@ -1,18 +1,66 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import theme from "../Theme/theme";
 import EventRegister from "./EventRegister";
 
-describe("Given an EventRegister form", () => {
-  describe("When it renders", () => {
-    test("Then it should render with 'email' input label for an email address", () => {});
-    render(
-      <ThemeProvider theme={theme}>
-        <EventRegister />
-      </ThemeProvider>
-    );
-    const inputLabel = "email";
+describe("Given a EventRegister component", () => {
+  describe("When rendered", () => {
+    test("Then it should show an input field for an email address", () => {
+      const expectedLabelText = /email/i;
 
-    expect(inputLabel).toHaveAttribute(inputLabel);
+      render(
+        <ThemeProvider theme={theme}>
+          <EventRegister />
+        </ThemeProvider>
+      );
+
+      const inputField = screen.getByLabelText(expectedLabelText);
+
+      expect(inputField).toBeInTheDocument();
+    });
+
+    test("Then it should show an input field for a password", () => {
+      const expectedLabelText = /password/i;
+
+      render(
+        <ThemeProvider theme={theme}>
+          <EventRegister />
+        </ThemeProvider>
+      );
+
+      const inputField = screen.getByLabelText(expectedLabelText);
+
+      expect(inputField).toBeInTheDocument();
+    });
+
+    test("Then it should show an input field for an image file", () => {
+      const expectedLabelText = /image/i;
+
+      render(
+        <ThemeProvider theme={theme}>
+          <EventRegister />
+        </ThemeProvider>
+      );
+
+      const inputField = screen.getByLabelText(expectedLabelText);
+
+      expect(inputField).toBeInTheDocument();
+    });
+
+    test("Then it should show a submit button with text 'Sign up'", () => {
+      const expectedButtonText = /sign up/i;
+
+      render(
+        <ThemeProvider theme={theme}>
+          <EventRegister />
+        </ThemeProvider>
+      );
+
+      const submitButton = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      expect(submitButton).toBeInTheDocument();
+    });
   });
 });
